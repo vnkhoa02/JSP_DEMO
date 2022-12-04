ba213bc9724f91c1d55ad3bca01e8b285196b0e1
khoavn <khoacaro96@gmail.com>
2022-12-05 01:43:34 +0700

    update code

--- src/main/java/com/vnk/jspdemo/EmployeeDao.java
+++ src/main/java/com/vnk/jspdemo/EmployeeDao.java
@@ -1,10 +1,12 @@
 package com.vnk.jspdemo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.vnk.jspdemo.Constant.Api;
 import com.vnk.jspdemo.Model.Employee;
 import com.vnk.jspdemo.StatusCode.StatusCode;
 import okhttp3.*;
+import org.json.JSONObject;
 
 public class EmployeeDao {
     ObjectMapper objectMapper = new ObjectMapper();
@@ -38,38 +40,13 @@ public class EmployeeDao {
 
         String url = urlBuilder.build().toString();
 
-        RequestBody formBody = new FormBody.Builder()
-                .add("name", employee.getName())
-                .add("age", String.valueOf(employee.getAge()))
-                .build();
+        MediaType mediaType = MediaType.parse("application/json;charset=utf-8");
 
-        Request request = new Request.Builder().
-                url(url).
-                post(formBody).
-                build();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", employee.getName());
+        jsonObject.put("age", employee.getAge());
 
-
-        StatusCode statusCode = new StatusCode();
-        try {
-            Response response = client.newCall(request).execute();
-            String jsonString = response.body().string();
-            statusCode = objectMapper.readValue(jsonString, StatusCode.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return statusCode;
-    }
-
-    public StatusCode updateEmployee(Employee employee) {
-        HttpUrl.Builder urlBuilder = HttpUrl.parse(Api.BASE_URL + Api.UPDATE_EMPLOYEE_DETAILS).newBuilder();
-
-        String url = urlBuilder.build().toString();
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("key", "C1B5")
-                .add("name", "abc")
-                .add("age", String.valueOf(19))
-                .build();
+        RequestBody formBody = RequestBody.create(mediaType, jsonObject.toString());
 
         Request request = new Request.Builder().
                 url(url).
@@ -87,6 +64,35 @@ public class EmployeeDao {
         return statusCode;
     }
 
+    public StatusCode updateEmployee(Employee employee) {
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Api.BASE_URL + Api.UPDATE_EMPLOYEE_DETAILS).newBuilder();
+
+        String url = urlBuilder.build().toString();
+        MediaType mediaType = MediaType.parse("application/json;charset=utf-8");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key", employee.getKey());
+        jsonObject.put("name", employee.getName());
+        jsonObject.put("age", employee.getAge());
+        RequestBody formBody = RequestBody.create(mediaType, jsonObject.toString());
+
+        Request request = new Request.Builder().
+                url(url).
+                put(formBody).
+                build();
+
+        StatusCode statusCode = new StatusCode();
+        String jsonString = "";
+        try {
+            Response response = client.newCall(request).execute();
+            jsonString = response.body().string();
+            statusCode = objectMapper.readValue(jsonString, StatusCode.class);
+        } catch (Exception e) {
+            statusCode.setResultMessage(jsonString);
+        }
+        return statusCode;
+    }
+
     public StatusCode deleteEmployee(String key) {
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Api.BASE_URL + Api.DELETE_EMPLOYEE_DETAILS).newBuilder();
 
@@ -100,6 +106,7 @@ public class EmployeeDao {
         try {
             Response response = client.newCall(request).execute();
             String jsonString = response.body().string();
+            System.out.println(jsonString);
             statusCode = objectMapper.readValue(jsonString, StatusCode.class);
         } catch (Exception e) {
             e.printStackTrace();
